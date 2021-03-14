f9b023cb66c97966690b666545f1d4c45735bcbc
gabruw <gabrielmarques.mg@gmail.com>
2021-03-13 21:25:44 -0300

    feat: Novos campos da entidade NGO

--- foster-pet-user/src/main/java/com/foster/pet/entity/NGO.java
+++ foster-pet-user/src/main/java/com/foster/pet/entity/NGO.java
@@ -8,8 +8,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.br.CNPJ;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,7 +31,24 @@ public class NGO implements Serializable {
 	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "entityName", nullable = false)
+	@Size(min = 1, max = 200, message = "O campo 'Nome da Entidade' deve conter entre 1 e 200 caracteres.")
+	private String entityName;
+
+	@Column(name = "tradeName", nullable = false)
+	@Size(min = 1, max = 200, message = "O campo 'Nome Fantasia' deve conter entre 1 e 200 caracteres.")
+	private String tradeName;
+
+	@CNPJ(message = "O campo 'CNPJ' é inválido.")
+	@Column(name = "cnpj", unique = true, nullable = true)
+	@Size(min = 19, max = 19, message = "O campo 'CNPJ' deve conter 19 caracteres.")
+	private String cnpj;
 
 	@ManyToMany(mappedBy = "ngo")
 	private List<Address> addresses;
+
+	@ManyToMany
+	@JoinTable(name = "ngo_employee", joinColumns = @JoinColumn(name = "ngo_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "employee_id", referencedColumnName = "id"))
+	private List<Employee> employee;
 }
