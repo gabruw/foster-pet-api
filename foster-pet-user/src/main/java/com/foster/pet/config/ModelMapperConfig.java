d86a589d8349d23ca2016e23d9352220298ee948
gabruw <gabrielmarques.mg@gmail.com>
2021-05-02 14:05:02 -0300

    feat: Remoção de mapper's não úteis

--- foster-pet-user/src/main/java/com/foster/pet/config/ModelMapperConfig.java
+++ foster-pet-user/src/main/java/com/foster/pet/config/ModelMapperConfig.java
@@ -5,13 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.foster.pet.conversor.authentication.AuthenticationCompanyPDTOToAuthentication;
-import com.foster.pet.conversor.authentication.AuthenticationPersonPDTOToAuthentication;
 import com.foster.pet.conversor.authentication.AuthenticationFRPDTOToJwtUser;
-import com.foster.pet.conversor.state.StateFRPDTOToState;
-import com.foster.pet.conversor.state.StatePDTOToState;
-import com.foster.pet.conversor.state.StateToStateFRPDTO;
-import com.foster.pet.conversor.state.StateToStateHRDTO;
-import com.foster.pet.conversor.state.StateToStateRDTO;
+import com.foster.pet.conversor.authentication.AuthenticationPersonPDTOToAuthentication;
 
 @Configuration
 public class ModelMapperConfig {
@@ -19,15 +14,6 @@ public class ModelMapperConfig {
 	@Bean
 	public ModelMapper modelMapper() {
 		ModelMapper modelMapper = new ModelMapper();
-
-		// State To
-		modelMapper.addConverter(new StateToStateRDTO());
-		modelMapper.addConverter(new StateToStateHRDTO());
-		modelMapper.addConverter(new StateToStateFRPDTO());
-
-		// To State
-		modelMapper.addConverter(new StatePDTOToState());
-		modelMapper.addConverter(new StateFRPDTOToState());
 
 		// Authentication To
 		modelMapper.addConverter(new AuthenticationFRPDTOToJwtUser());
