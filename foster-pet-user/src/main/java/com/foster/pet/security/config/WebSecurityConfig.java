3b99bf081b6a8b992de08f23baafd8ef1573ffba
gabruw <gabrielmarques.mg@gmail.com>
2021-05-04 19:23:49 -0300

    feat: Correção do Cors Filter

--- foster-pet-user/src/main/java/com/foster/pet/security/config/WebSecurityConfig.java
+++ foster-pet-user/src/main/java/com/foster/pet/security/config/WebSecurityConfig.java
@@ -79,7 +79,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public CorsFilter corsFilter() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
 		config.addAllowedOrigin("*");
 		config.addAllowedHeader("*");
 		config.addAllowedMethod("GET");
